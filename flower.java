5c617d8797c12dc535c6c557cdc95fb68c28ea77
Mustafa Aslanov <44026469+aslanovmustafa@users.noreply.github.com>
2020-04-08 22:22:35 +0400

    Update flower.java

--- flower.java
+++ flower.java
@@ -1,14 +1,15 @@
 
 public class flower {
+
 	  public void plant() {
-		    System.out.println("Robot planted a flower at:/n"
-		    		+ "X: %d"
-		    		+ "\nY: %d");
+		    System.out.println("Robot planted a flower at:\n"
+		    		+ "X: " + synch.X
+		    		+ "\nY: " + synch.Y);
 		  }
 		  
 		  public void reap() {
-			  System.out.println("Robot reaped a flower at:/n"
-			    		+ "X: %d"
-			    		+ "\nY: %d");
+			  System.out.println("Robot reaped a flower at:\n"
+			    		+ "X: " + synch.X
+			    		+ "\nY: " + synch.Y);
 		  }
 }
