79794e07c79f6e847c529b5d5a75adce5dbb7894
Mustafa Aslanov <44026469+aslanovmustafa@users.noreply.github.com>
2020-04-08 22:24:41 +0400

    Add files via upload

--- move.java
+++ move.java
@@ -1,16 +1,27 @@
-public class move {//the variables here are temporary, later on gotta make them global static
+public class move {
 	
-	
-	
+
 	  public void forward() {
+		  
+		  if (synch.North) synch.Y++;
+		  else if (synch.East) synch.X++;
+		  else if (synch.South) synch.Y--;
+		  else if (synch.West) synch.X--;
+	  
 		    System.out.println("Robot moved forward. New position:\n"
-		    		+ "X:" + "here will be x pos"
-		    		+ "\nY:" + "here will be y pos");
+		    		+ "X:" + synch.X
+		    		+ "\nY:" + synch.Y);
 		  }
 	  
 	  public void back() {
+		  
+		  if (synch.North) synch.Y--;
+		  else if (synch.East) synch.X--;
+		  else if (synch.South) synch.Y++;
+		  else if (synch.West) synch.X++;
+		  
 			System.out.println("Robot moved backward. New position:\n"  
-					+ "X:" + "here will be x pos" 
-					+ "\nY:" + "here will be y pos");
+					+ "X:" + synch.X
+		    		+ "\nY:" + synch.Y);
 			}
 }
